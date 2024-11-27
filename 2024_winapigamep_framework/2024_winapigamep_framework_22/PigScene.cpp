bd5ef7bdb732dc30f467ff648e9d466d8b7df749
yogurt-31 <syjang070813@gmail.com>
2024-11-27 09:32:50 +0900

    [20241127] 에러나잉

--- 2024_winapigamep_framework/2024_winapigamep_framework_22/PigScene.cpp
+++ 2024_winapigamep_framework/2024_winapigamep_framework_22/PigScene.cpp
@@ -8,17 +8,16 @@
 
 void PigScene::Init()
 {
-	TileObject* tile1 = new TileObject;
-	AddObject(tile1, LAYER::DEFAULT);
-
 	Agent* agent = new Agent;
 	AddObject(agent, LAYER::DEFAULT);
 	agent->GetTransform()->SetPosition({ SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2 });
 
-	Transform* trm = tile1->GetTransform();
-	trm->SetPosition({ SCREEN_WIDTH / 2 - trm->GetScale().x * 1.5f, SCREEN_HEIGHT / 2 - trm->GetScale().y * 0.5f });
 
-	tile1->GetTransform()->SetPosition({ SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 });
+	for (int i = 0; i < 10; ++i) {
+		TileObject* tile = new TileObject;
+		AddObject(tile, LAYER::DEFAULT);
 
+		tile->GetTransform()->SetPosition({ SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + i*256 });
+	}
 	//tile1->GetComponent<SpriteRenderer>()->CreateTexture(L"Texture\\bishojo1_resize.bmp", L"bishojo1");
 }
