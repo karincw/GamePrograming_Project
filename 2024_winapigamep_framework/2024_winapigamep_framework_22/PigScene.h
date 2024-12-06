2f9a94c7a2b8b81910df1ec68aca86bafd61eb1d
yogurt-31 <syjang070813@gmail.com>
2024-12-07 03:34:19 +0900

    [20241207] 맵 대공사

--- 2024_winapigamep_framework/2024_winapigamep_framework_22/PigScene.h
+++ 2024_winapigamep_framework/2024_winapigamep_framework_22/PigScene.h
@@ -23,8 +23,8 @@ class PigScene :
 public:
 	void Init() override;
 	void Render(HDC _hdc) override;
-	void CreateTile(Vec2 vec, TILE tileType);
-	void CreateTrap(Vec2 vec, ATKTRAP atkTrap);
-	Vec2 CreateVerticalTileGroup(Vec2 startVec, TILE tileType, int x, int y, bool isDown, bool isJump); 
-	Vec2 CreateHorizontalTileGroup(Vec2 startVec, TILE tileType, int x, int y, bool isRight, bool isJump);
+	Object* CreateTile(Vec2 vec, TILE tileType, int plusX = 0, int plusY = 0);
+	void CreateTrap(Vec2 vec, ATKTRAP atkTrap, int plusX = 0, int plusY = 0);
+	Object* CreateVerticalTileGroup(Vec2* startVec, TILE tileType, int x, int y, bool isDown, bool isJump); 
+	Object* CreateHorizontalTileGroup(Vec2* startVec, TILE tileType, int x, int y, bool isRight, bool isJump);
 };
